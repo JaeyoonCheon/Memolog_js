dc03c77eb81d897c001b50f30d7eb2284bd7ba89
JaeyoonCheon <cjswodbs50@gmail.com>
2023-02-16 02:07:54 +0900

    FEAT / 재사용 가능한 베이스 버튼 작성

---
+++ components/buttons/BaseButton.jsx
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import React from "react";
+
+const BaseButton = ({ label, onPress, secondary = false }) => {
+  return (
+    <TouchableOpacity style={styles({ secondary }).block} onPress={onPress}>
+      <Text style={styles({ secondary }).label}>{label}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default BaseButton;
+
+const styles = (props) =>
+  StyleSheet.create({
+    block: {
+      height: 48,
+      alignItems: "center",
+      justifyContent: "center",
+
+      borderRadius: 5,
+      backgroundColor: props.secondary ? "#FFFFFF" : "#22BCCE",
+      borderWidth: props.secondary ? 1 : undefined,
+      borderColor: props.secondary ? "#747878" : undefined,
+    },
+    label: {
+      fontSize: 16,
+
+      color: props.secondary ? "#000000" : "#FFFFFF",
+    },
+  });
