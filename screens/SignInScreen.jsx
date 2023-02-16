1ce6ae16b9aac9d7cba4cc844d01a4b42363e48a
JaeyoonCheon <cjswodbs50@gmail.com>
2023-02-16 17:42:56 +0900

    ADD / 체크박스, 폼 검증 라이브러리 추가 1. 체크박스 - react-native-bouncy-checkbox 2. 폼 유효성 검증 - react-hook-form

--- screens/SignInScreen.jsx
+++ screens/SignInScreen.jsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
+import BouncyCheckbox from "../node_modules/react-native-bouncy-checkbox/build/dist/BouncyCheckbox";
 
 import BaseHeader from "../components/headers/BaseHeader";
 import BaseTextField from "../components/textfields/BaseTextField";
@@ -12,6 +13,15 @@ const SignInScreen = () => {
       <Text>SignInScreen</Text>
       <BaseTextField></BaseTextField>
       <BaseTextField></BaseTextField>
+      <BouncyCheckbox
+        size={20}
+        fillColor="#22BCCE"
+        text="자동로그인"
+        iconStyle={{ borderRadius: 5 }}
+        innerIconStyle={{ borderRadius: 5 }}
+        textStyle={{ textDecorationLine: "none", fontSize: 12 }}
+        textContainerStyle={{ marginLeft: 8 }}
+      ></BouncyCheckbox>
       <BaseButton label="로그인하기"></BaseButton>
     </View>
   );
