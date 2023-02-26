ed0a1c66b9794c65d625800c4579ecf52f64293a
JaeyoonCheon <cjswodbs50@gmail.com>
2023-02-26 23:30:35 +0900

    ETC / IP 변경

--- api/client.js
+++ api/client.js
@@ -1,7 +1,9 @@
 import axios from "axios/index";
 
 // wsl2 서버 실행 시 wsl2의 IP address를 대응 후 adb 포트 연동 필요
-const baseURL = __DEV__ ? "http://172.25.158.19:3367" : "http://localhost:3367";
+const baseURL = __DEV__
+  ? "http://192.168.234.141:3367"
+  : "http://localhost:3367";
 
 const client = axios.create({
   baseURL,
