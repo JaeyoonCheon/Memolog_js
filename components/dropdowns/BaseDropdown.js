521fdbff6460ac125b0e6b14fc0bd87255561013
JaeyoonCheon <cjswodbs50@gmail.com>
2023-03-28 22:44:12 +0900

    ADD / 커스텀 드롭다운 컴포넌트 간략 구현

--- components/dropdowns/BaseDropdown.js
+++ components/dropdowns/BaseDropdown.js
@@ -1,14 +1,71 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Modal,
+  FlatList,
+} from "react-native";
 import React from "react";
 
-const BaseDropdown = () => {
+const BaseDropdown = ({
+  list,
+  selected,
+  open,
+  handleSelection,
+  toggleOpen,
+}) => {
+  const DropdownItem = ({ item }) => {
+    return (
+      <TouchableOpacity onPress={toggleOpen}>
+        <Text>{item.label}</Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View>
-      <Text>BaseDropdown</Text>
-    </View>
+    <TouchableOpacity style={styles.block} onPress={toggleOpen}>
+      <Text style={styles.mainLabel}>{selected}</Text>
+      <Modal visible={open} transparent animationType="none">
+        <View style={styles.dropdown}>
+          <FlatList
+            data={list}
+            renderItem={DropdownItem}
+            keyExtractor={(item) => item.value}
+          ></FlatList>
+        </View>
+      </Modal>
+    </TouchableOpacity>
   );
 };
 
 export default BaseDropdown;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {
+    paddingHorizontal: 4,
+    paddingVertical: 2,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+
+    backgroundColor: "#FFFFFF",
+    borderWidth: 1,
+    borderColor: "blue",
+    zIndex: 1,
+  },
+  mainLabel: {
+    textAlign: "center",
+  },
+  dropdown: {
+    position: "absolute",
+
+    borderWidth: 1,
+    borderColor: "red",
+  },
+  itemBlock: {
+    backgroundColor: "#FFFFFF",
+    borderWidth: 1,
+    borderColor: "#000000",
+  },
+});
--- hooks/useDropdown.js
+++ hooks/useDropdown.js
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 
 export default function useDropdown(items) {
   const [list, setList] = useState(items);
-  const [selected, setSelected] = useState(items[0]);
+  const [selected, setSelected] = useState(items[0].label);
+  const [open, setOpen] = useState(false);
 
   const handleSelection = (item) => {
     setSelected(item);
   };
+  const toggleOpen = () => {
+    setOpen(!open);
+  };
 
-  return [list, selected, handleSelection];
+  return { list, selected, open, handleSelection, toggleOpen };
 }
