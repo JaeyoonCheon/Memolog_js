96c1c456a04c8409d85687fb9068beb4979f858c
JaeyoonCheon <cjswodbs50@gmail.com>
2023-05-14 23:49:22 +0900

    ADD / 회원가입-프로필 작성 화면 컴포넌트 생성

---
+++ screens/MakeProfileScreen.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const MakeProfileScreen = () => {
+  return <View style={styles.block}></View>;
+};
+
+export default MakeProfileScreen;
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+  },
+});
